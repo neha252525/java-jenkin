591e3451b9fcea862dc03941606589deb119a7de
neha <por.neha@gmail.com>
2024-11-12 14:34:39 +0000

    added java file

---
+++ jenkinjava.java
@@ -0,0 +1,8 @@
+public class Demo{
+    public static void main (String [] args){
+    System.out.println ("Hello from Demo");
+        System.out.println ("Hello to Sweta from demo java");
+         System.out.println ("Hello to Jayant from demo java");
+               System.out.println ("Hello to Takshal from demo java");
+    }
+} 
