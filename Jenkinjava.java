0a2087aefb518e597ebf7065deded402919b078d
neha <por.neha@gmail.com>
2024-11-12 14:58:55 +0000

    Renamed

--- Jenkinjava.java
+++ Jenkinjava.java
@@ -1,4 +1,4 @@
-public class Demo{
+public class Jenkinjava{
     public static void main (String [] args){
     System.out.println ("Hello from Demo");
         System.out.println ("Hello to Sweta from demo java");
